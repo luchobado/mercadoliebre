1c46207809669d8929ca881ab64a96acdca2b894
luchobado <luchobadox@gmail.com>
2023-09-04 20:43:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public');
+
+//variable de entorno
+const PORT= process.env.PORT || 3050;
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+})
+
+app.get('/signin', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/signin.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
